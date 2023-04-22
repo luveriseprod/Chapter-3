63f010eb624e529c799bf6eaa0d0ee0ece141f3c
LRDev <luveriseprod@gmail.com>
2023-04-22 17:15:36 +0600

    Ep.3

---
+++ 3-2.js
@@ -0,0 +1,11 @@
+// Название статьи
+
+let article1 = "Зачем я изучаю JS";
+let article2 = "Почему именно этот язык";
+
+console.log("Название первой статьи:");
+console.log(article1);
+
+console.log("Название второй статьи:");
+console.log(article2); 
+
