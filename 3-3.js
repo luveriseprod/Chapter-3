728fd09daa73bc0354301cd1d29ab16cae89f558
LRDev <luveriseprod@gmail.com>
2023-04-23 13:39:45 +0600

    3-3

---
+++ 3-3.js
@@ -0,0 +1,30 @@
+let str = "Hello";
+let strNumber = "5";
+let emptyString = "";
+let number = 6;
+let boolean =  true;
+
+console.log("Преобразование к числу");
+console.log(Number(str));
+console.log(Number(strNumber));
+console.log(Number(emptyString));
+console.log(Number(number));
+console.log(Number(boolean));
+console.log("_____________________________");
+
+console.log("Преобразование к строке");
+console.log(String(str));
+console.log(String(strNumber));
+console.log(String(emptyString));
+console.log(String(number));
+console.log(String(boolean));
+console.log("_____________________________");
+
+console.log("Преобразование к лог. выражению");
+console.log(Boolean(str));
+console.log(Boolean(strNumber));
+console.log(Boolean(emptyString));
+console.log(Boolean(number));
+console.log(Boolean(boolean));
+console.log("_____________________________");
+
