687d15b565fbc2bf649ef2558c291e7bf27686cb
LRDev <93220194+luveriseprod@users.noreply.github.com>
2023-10-08 13:28:08 +0600

    3-17

---
+++ 3-17.js
@@ -0,0 +1,25 @@
+function Article (name, text) {
+    this.name = name;
+    this.text = text;
+};
+
+function User (name, articles) {
+    this.name = name;
+    this.articles = articles
+};
+
+User.prototype.logArticles = function() {
+    this.articles.forEach((article, i) => {
+        console.log(`${i+ 1}. ${article.name}`);
+    });
+}
+
+
+let article1 = new Article("React", "React is nice");
+let article2 = new Article("Angular", "Angular is also nice");
+
+let user = new User("Alex", [article1, article2])
+
+console.log(user);
+
+user.logArticles()
